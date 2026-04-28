14c0e67945f2cbe31f39c613698d5d8d4573fd1c
Gabriel H. Carraretto <gabriel.carraretto@finconsgroup.com>
2026-04-28 15:23:45 +0200

    feat: enhance C language support with additional standard types and PascalCase identifier handling

--- src/languages/c.js
+++ src/languages/c.js
@@ -5,6 +5,7 @@ const {
   scanLineComment,
   scanDoubleQuoted,
   scanSingleQuoted,
+  buildExclusionChecker,
 } = require("../scanner");
 
 const keywords = new Set([
@@ -13,7 +14,15 @@ const keywords = new Set([
   "inline", "int", "long", "register", "restrict", "return", "short",
   "signed", "sizeof", "static", "struct", "switch", "typedef", "union",
   "unsigned", "void", "volatile", "while",
+  // standard types
+  "size_t", "ssize_t", "ptrdiff_t", "uintptr_t", "intptr_t",
+  "uint8_t", "uint16_t", "uint32_t", "uint64_t",
+  "int8_t", "int16_t", "int32_t", "int64_t",
+  "bool", "FILE", "NULL", "true", "false",
 ]);
+
+// PascalCase identifiers are user-defined types (Arena, Entry, Intern, ...)
+const PASCAL_RE = /\b([A-Z][a-zA-Z0-9]*[a-z][a-zA-Z0-9]*)\b/g;
 
 function scan(text) {
   const segs = [];
@@ -30,8 +39,18 @@ function scan(text) {
   return segs;
 }
 
+function applyKeys(text, doc, excluded, ranges, span) {
+  const isExcluded = buildExclusionChecker(excluded);
+  PASCAL_RE.lastIndex = 0;
+  for (const m of text.matchAll(PASCAL_RE)) {
+    if (!isExcluded(m.index))
+      ranges.teal.push(span(doc, m.index, m.index + m[0].length));
+  }
+}
+
 module.exports = {
   ids: ["c"],
   keywords,
   scan,
+  applyKeys,
 };
